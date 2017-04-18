7520b912fceb566a7fadcacce23dacec7cee3db5
Stefan Milenkovic <stefan.milenkovic@vendemore.com>
2017-04-18 23:30:34 +0200

    Retrieval of racks and putting them together with issues

--- js/controllers/sidebar-ctrl.js
+++ js/controllers/sidebar-ctrl.js
@@ -4,7 +4,6 @@ angular.module('bikeApp').controller('SidebarCtrl', ['$scope', '$rootScope', '$c
 function SidebarCtrl($scope, $rootScope, $cookieStore, $timeout, $controller, leafletBoundsHelpers, BikeIssueService){
 
     $scope.filterMarkersForIssues = function (marker) {
-        console.log("Checking: "+JSON.stringify(marker));
         return marker.issue_type !== undefined;
     };
 };
