21d614c104d1962fbe8fe90f75b507c9b43183ef
Stefan Milenkovic <stefan.milenkovic@vendemore.com>
2017-04-19 14:35:06 +0200

    Set HTTPS on many bad links

--- js/controllers/overview-ctrl.js
+++ js/controllers/overview-ctrl.js
@@ -50,9 +50,9 @@ function OverviewCtrl($scope, $rootScope, $cookieStore, $timeout, $controller, l
     ]);
 
     $scope.tiles = {
-      url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       options: {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
         maxZoom: 18
       }
     };
